db1e54ae82d1de586e3f4d7ebf89f6407bf5d87e
islamdeveloper <islamprojectmanager@gmail.com>
2023-04-27 10:20:44 +0400

    Initial Commit

---
+++ NY TimesTests/NY_TimesTests.swift
@@ -0,0 +1,33 @@
+//
+//  NY_TimesTests.swift
+//  NY TimesTests
+//
+//  Created by Saeed Alshamsi on 27/04/2023.
+//
+
+import XCTest
+@testable import NY_Times
+
+class NY_TimesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
