fa04aba53e36238c990a34fc766389ec78474a94
islamdeveloper <islamprojectmanager@gmail.com>
2023-04-28 19:35:02 +0400

    first commit

---
+++ NY Times/Helper/Coordinator/StoryBoardName.swift
@@ -0,0 +1,13 @@
+//
+//  StoryboardName.swift
+//  NY Times
+//
+//  Created by Islam Ahmed on 27/04/2023.
+
+import Foundation
+struct StoryBoardName {
+    static let start = "Main"
+}
+struct NibNameIdentifier {
+    static let topNavigation = "TopNavigation"
+}
